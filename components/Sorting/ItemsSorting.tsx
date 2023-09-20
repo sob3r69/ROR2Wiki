c69adc7b40f4442400e3e46953f082dc76c19beb
sob3r69 <iamnotsob3r@gmail.com>
2023-09-20 14:32:35 +0300

    Added functionality to filter items by rarity

--- components/Sorting/ItemsSorting.tsx
+++ components/Sorting/ItemsSorting.tsx
@@ -1,40 +1,51 @@
-import { itemsAtom } from '@/store/Atoms';
+import { itemsAtom, initAtom } from '@/store/Atoms';
 import { useAtom } from 'jotai';
-import React from 'react';
 import style from './sorting.module.css';
 
 const ItemsSorting = () => {
   const [items, setItems] = useAtom(itemsAtom);
-  const testData = items.concat();
+  const [initialItems] = useAtom(initAtom);
+  const testData = initialItems.concat();
 
   const sortItemsUp = () => {
     const sortedItems = testData.sort((a, b) => {
       return a[1].RARITY - b[1].RARITY;
     });
     setItems(sortedItems);
-    console.log(items);
   };
   const sortDataDown = () => {
     const sortedItems = testData.sort((a, b) => {
       return b[1].RARITY - a[1].RARITY;
     });
     setItems(sortedItems);
-    console.log(items);
   };
 
-  // const sortWhite = () => {
-  //   const sortedItems = testData.filter((item) => {
-  //     if (item[1].RARITY === 0) return item;
-  //   });
-  //   setItems(sortedItems);
-  //   console.log(items);
-  // };
+  const sortWhite = () => {
+    const sortedItems = testData.filter((item) => {
+      if (item[1].RARITY === 0) return item;
+    });
+    setItems(sortedItems);
+  };
+  const sortGreen = () => {
+    const sortedItems = testData.filter((item) => {
+      if (item[1].RARITY === 1) return item;
+    });
+    setItems(sortedItems);
+  };
+  const sortRed = () => {
+    const sortedItems = testData.filter((item) => {
+      if (item[1].RARITY === 3) return item;
+    });
+    setItems(sortedItems);
+  };
 
   return (
     <div className={style.container}>
       <button onClick={sortItemsUp}>RARITY -+</button>
       <button onClick={sortDataDown}>RARITY +-</button>
-      {/* <button onClick={sortWhite}>white</button> */}
+      <button onClick={sortWhite}>white</button>
+      <button onClick={sortGreen}>green</button>
+      <button onClick={sortRed}>red</button>
     </div>
   );
 };
--- store/Atoms.ts
+++ store/Atoms.ts
@@ -4,4 +4,4 @@ import json from '@/data/Items.json';
 const initialData = Object.entries(json);
 
 export const itemsAtom = atom(initialData);
-export const testAtom = atom(0);
+export const initAtom = atom(initialData.concat());
